b7b238e89a400fed466a1d3f2541b4ee8caaa1d9
nathaniellr <105675018+nathaniellr@users.noreply.github.com>
2022-05-22 14:21:03 -0400

    Update hidenav.js

--- cbn/hidenav.js
+++ cbn/hidenav.js
@@ -1,25 +1,34 @@
-const nav = document.querySelector('.super-navbar')
-const { height } = nav.getBoundingClientRect()
 
-const setRangeBetween = (min, max) => number =>
-  Math.min(Math.max(number, min), max)
-
-const limitTransformFor = setRangeBetween(0, height)
-
-let lastScrollY = 0
-
-const handleScroll = () => {
-  let { scrollY } = window
-  let navTransform = parseInt(nav.style.transform.replace(/[^0-9\.]/g, ''), 10)
-  let diff = limitTransformFor(navTransform - (lastScrollY - scrollY))
-  if(scrollY >= lastScrollY) {
-    nav.style.transform = `translateY(-${ diff > 0 ? diff : limitTransformFor(scrollY) }px)`
-  } else { 
-    nav.style.transform = `translateY(-${diff}px)`
+setTimeout(function(){
+  const nav = document.querySelector('.super-navbar')
+  const { height } = nav.getBoundingClientRect()
+  
+  const setRangeBetween = (min, max) => number =>
+    Math.min(Math.max(number, min), max)
+  
+  const limitTransformFor = setRangeBetween(0, height)
+  
+  let lastScrollY = 0
+  
+  const handleScroll = () => {
+    let { scrollY } = window
+    let navTransform = parseInt(nav.style.transform.replace(/[^0-9\.]/g, ''), 10)
+    let diff = limitTransformFor(navTransform - (lastScrollY - scrollY))
+    if(scrollY >= lastScrollY) {
+      nav.style.transform = `translateY(-${ diff > 0 ? diff : limitTransformFor(scrollY) }px)`
+    } else { 
+      nav.style.transform = `translateY(-${diff}px)`
+    }
+    
+    lastScrollY = scrollY
   }
   
-  lastScrollY = scrollY
-}
+  
+  window.addEventListener('scroll', handleScroll)
 
-window.addEventListener('scroll', handleScroll)
+}, 500); 
+
+
+
+
 
