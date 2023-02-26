9d046dd193f797235cecd111065983010f8d5fc9
nathaniellr <105675018+nathaniellr@users.noreply.github.com>
2023-02-26 08:55:53 -0500

    Update super-embed.js

--- cbn/super-embed.js
+++ cbn/super-embed.js
@@ -34,3 +34,13 @@ if (document.querySelector(SELECTOR)) {
 }
 });
 observer.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
+
+const video = document.getElementById("video");
+
+setTimeout(() => {
+  video.classList.add("slide-in");
+}, 500); /* Fade in after 0.5 seconds */
+
+setTimeout(() => {
+  video.classList.remove("slide-in");
+}, 5000); /* Fade out after 5 seconds */
