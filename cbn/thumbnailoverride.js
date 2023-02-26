73b1249e1b4ce0781b8524781f6b2cda0fb80cbd
nathaniellr <105675018+nathaniellr@users.noreply.github.com>
2023-02-26 09:34:08 -0500

    Update thumbnailoverride.js

--- cbn/thumbnailoverride.js
+++ cbn/thumbnailoverride.js
@@ -1,13 +1,21 @@
-document.addEventListener('click', function(event) {
-  if (event.target.matches('div#block-3d0e68edaa7046c597a90af1083d56ff')) {
+function attachEventListeners() {
+  document.querySelector('div#block-3d0e68edaa7046c597a90af1083d56ff').addEventListener('click', function() {
     window.open('https://dribbble.com/shots/20706559-Calibre-Redesign', '_blank');
-  }
+  });
 
-  if (event.target.matches('div#block-ed5790ff6d1f46a19387a2315d3540c5')) {
+  document.querySelector('div#block-ed5790ff6d1f46a19387a2315d3540c5').addEventListener('click', function() {
     window.open('https://dribbble.com/shots/20759042-AI-Company', '_blank');
-  }
+  });
 
-  if (event.target.matches('div#block-62d926c915954e72a3d6c5ef95018f1a')) {
+  document.querySelector('div#block-62d926c915954e72a3d6c5ef95018f1a').addEventListener('click', function() {
     window.open('https://dribbble.com/shots/20305837-Photography-Website', '_blank');
-  }
+  });
+}
+
+window.onload = function() {
+  attachEventListeners();
+}
+
+window.addEventListener('load', function() {
+  attachEventListeners();
 });
