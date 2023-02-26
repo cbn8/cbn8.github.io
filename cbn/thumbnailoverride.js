4b7ef30b3b69cc570a11d57ed01ea245d1a8e1a2
nathaniellr <105675018+nathaniellr@users.noreply.github.com>
2023-02-26 09:18:41 -0500

    push

--- cbn/thumbnailoverride.js
+++ cbn/thumbnailoverride.js
@@ -1,11 +1,13 @@
-document.querySelector('div#block-3d0e68edaa7046c597a90af1083d56ff').addEventListener('click', function() {
-  window.open('https://dribbble.com/shots/20706559-Calibre-Redesign', '_blank');
-});
+window.onload = function() {
+  document.querySelector('div#block-3d0e68edaa7046c597a90af1083d56ff').addEventListener('click', function() {
+    window.open('https://dribbble.com/shots/20706559-Calibre-Redesign', '_blank');
+  });
 
-document.querySelector('div#block-ed5790ff6d1f46a19387a2315d3540c5').addEventListener('click', function() {
+  document.querySelector('div#block-ed5790ff6d1f46a19387a2315d3540c5').addEventListener('click', function() {
     window.open('https://dribbble.com/shots/20759042-AI-Company', '_blank');
   });
 
   document.querySelector('div#block-62d926c915954e72a3d6c5ef95018f1a').addEventListener('click', function() {
     window.open('https://dribbble.com/shots/20305837-Photography-Website', '_blank');
   });
+}
--- cbn/welcomevideo.js
+++ cbn/welcomevideo.js
@@ -1,9 +1,12 @@
-const video = document.getElementById("video");
-
-setTimeout(() => {
-  video.classList.add("slide-in");
-}, 500); /* Fade in after 0.5 seconds */
-
-setTimeout(() => {
-  video.classList.remove("slide-in");
-}, 5000); /* Fade out after 5 seconds */
+window.onload = function() {
+    const video = document.getElementById("video");
+  
+    setTimeout(() => {
+      video.classList.add("slide-in");
+    }, 500); /* Fade in after 0.5 seconds */
+  
+    setTimeout(() => {
+      video.classList.remove("slide-in");
+    }, 5000); /* Fade out after 5 seconds */
+  }
+  
