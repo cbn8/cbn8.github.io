ce92b6037a357f84e6ec998249251be9ba983710
nathaniellr <105675018+nathaniellr@users.noreply.github.com>
2023-02-26 06:58:47 -0500

    Update thumbnailoverride.js

--- cbn/thumbnailoverride.js
+++ cbn/thumbnailoverride.js
@@ -1,3 +1,3 @@
-document.querySelector('.block-3d0e68edaa7046c597a90af1083d56ff').addEventListener('click', function() {
+document.querySelector('div#block-3d0e68edaa7046c597a90af1083d56ff.notion-collection-card.gallery.no-click').addEventListener('click', function() {
   window.open('https://dribbble.com/shots/20706559-Calibre-Redesign', '_blank');
 });
