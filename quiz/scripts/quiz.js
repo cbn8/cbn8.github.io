64489b8aaedd02ebede74c9425a6e085f9ff5719
nathaniellr <105675018+nathaniellr@users.noreply.github.com>
2023-09-11 19:33:44 -0400

    Update quiz.js

--- quiz/scripts/quiz.js
+++ quiz/scripts/quiz.js
@@ -10,7 +10,7 @@ const QuizApp = {
     // Add more objects for additional CSV files if needed
   ],
 
-  defaultCsvFileName: "https://nathaniellr.github.io/quiz/data/webdev-1.csv",
+  defaultCsvFileName: "webdev-1.csv",
 
   currentQuestion: 0,
   score: 0,
